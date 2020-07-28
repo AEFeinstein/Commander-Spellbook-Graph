cee3106bcfd201177cc435bd9bebfb1f0d45a8a8
AFeinstein <afeinstein@alarm.com>
2020-07-28 16:46:49 -0400

    Just messing around

--- comboMapper.py
+++ comboMapper.py
@@ -20,9 +20,11 @@ def recursiveSearch(comboGraph, subGraph, key, rLevel, minWeight):
         # Move it from the main graph to the subgraph
         subGraph[key] = comboGraph.pop(key)
         # For all edges from that vertex
-        for rKey in subGraph[key].keys():
+        for rKey in list(subGraph[key].keys()):
+            if subGraph[key][rKey] < minWeight:
+                subGraph[key].pop(rKey)
             # If we haven't recursed too far and the edge is weighty enough
-            if(rLevel > -1 and subGraph[key][rKey] >= minWeight):
+            elif(rLevel > 1):
                 # Keep searching
                 recursiveSearch(comboGraph, subGraph,
                                 rKey, rLevel-1, minWeight)
@@ -36,12 +38,23 @@ if __name__ == "__main__":
     comboGraph = dict({})
 
     # Load the JSON from the web
-    with urllib.request.urlopen("https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:O&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E") as url:
-        data = json.loads(url.read().decode())
+    with open('values_batchGet.json') as f:
+        data = json.load(f)
+    # with urllib.request.urlopen("https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:O&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E") as url:
+    #     data = json.loads(url.read().decode())
         # Pull out the array of combos (rows in the spreadsheet). This has cards, colors, conditions, and outcomes
         values = data['valueRanges'][0]['values']
         # For each combo
         for combo in values:
+
+            # Only graph three card combos
+            # cardsInCombo = 0
+            # for cardIdx in range(10):
+            #     if combo[cardIdx]:
+            #         cardsInCombo = cardsInCombo + 1
+            # if cardsInCombo > 3:
+            #     continue
+
             # For each card in the combo (up to the first ten elements)
             for cardIdx in range(10):
                 # If the card exists
@@ -69,7 +82,7 @@ if __name__ == "__main__":
     # Now that the full graph is built, do a recursive search for all cards no
     # more than three edges of weight one away from Sliver Queen
     queenGraph = dict({})
-    recursiveSearch(comboGraph, queenGraph, "Sliver Queen", 2, 1)
+    recursiveSearch(comboGraph, queenGraph, "Sliver Queen", 2, 2)
 
     # Write the graphviz for this graph
     with open("combo.gv", "w") as fout:
